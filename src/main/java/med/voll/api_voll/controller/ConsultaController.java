649c6b52d36f8fc49885ed2ec1ec32d5348f9d2a
Davi Costa <davim.costa@outlook.com>
2024-06-23 22:57:08 -0300

    testes automatizados

--- src/main/java/med/voll/api_voll/controller/ConsultaController.java
+++ src/main/java/med/voll/api_voll/controller/ConsultaController.java
@@ -1,17 +1,18 @@
 package med.voll.api_voll.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api_voll.domain.consulta.AgendaDeConsultas;
 import med.voll.api_voll.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api_voll.domain.consulta.DadosCancelamentoConsulta;
-import med.voll.api_voll.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultaController {
 
     @Autowired
